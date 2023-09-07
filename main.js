1f285de16db9605ba390bf8ad0ecc584a10e7eea
name <email@email.ru>
2023-09-07 13:54:57 +0300

    2

---
+++ main.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'Hello ISiP-25!'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
